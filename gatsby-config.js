70ac8f6ea3a024f2da8e5d807e3a80dee39f874d
hyujikoh <dvum0045@gmail.com>
2022-06-28 16:45:42 +0900

    그래프 ql 로 데이터 가져와서 레이아웃에 출력

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "My First Gatsby Site",
+    description:"이 사이트는 백엔드 스쿨 1기교육생 오현직학생 소유입니다."
+  },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp"
