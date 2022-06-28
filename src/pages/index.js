a00d6e3c4bf7af25f9231d6b297d1b9da308ff46
hyujikoh <dvum0045@gmail.com>
2022-06-28 11:46:46 +0900

    index  페이지 내용 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+const IndexPage =() =>{
+  return (
+<main>
+    <title>Home Page???</title>
+    <h1>Home Page!!!</h1>
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+</main>
+
+  );
+};
+
+
+export default IndexPage;
