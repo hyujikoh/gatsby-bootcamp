71121fd3543b617844be5ffa8caf0de25ac69e7a
hyujikoh <dvum0045@gmail.com>
2022-06-28 22:30:48 +0900

    블로그 포스트 페이지 추가

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 import { Link,useStaticQuery,graphql } from "gatsby";
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 const BlogPage = () => {
