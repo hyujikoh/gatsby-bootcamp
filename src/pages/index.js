3686b30da3dc3f515f21372b1002f5670821d089
hyujikoh <dvum0045@gmail.com>
2022-06-28 14:07:33 +0900

    헤더파일 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,14 @@
 import React from "react"
-import { Link } from "gatsby";
+import Head from "../components/head";
 const IndexPage =() =>{
   return (
-<main>
-    <title>Home Page???</title>
+    <main>
+      <title>Home Page???</title>
 
-    <div style={{display:"flex",gap:20}}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/">홈</Link>
-</div>
-    <h1>Home Page!!!</h1>
+      <Head /> 
+      <h1>Home Page!!!</h1>
 
-</main>
+    </main>
 
   );
 };
