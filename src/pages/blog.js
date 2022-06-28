401b7ee0fed5bd4a08ae331619f3c2b25d825c50
hyujikoh <dvum0045@gmail.com>
2022-06-28 17:37:10 +0900

    블로그 포스트 리스트

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Link,useStaticQuery,graphql } from "gatsby";
 const BlogPage = () => {
+
+    const data = useStaticQuery(graphql`
+    query{
+        allFile {
+       nodes {
+         name
+       }
+     }
+   }
+  `);
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My cool posts will go in here</p>
+        <ul>
+        {data.allFile.nodes.map((node)=> (
+            <li key= {node.name}> {node.name}</li>
+        ))}
+            
+        </ul>
+
+      {JSON.stringify(data)}
     </Layout>
   )
 }
