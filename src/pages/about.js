b163dd1d63501846292a704732d2be995a98fa36
hyujikoh <dvum0045@gmail.com>
2022-06-28 14:07:43 +0900

    헤더파일

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { Link } from "gatsby";
+import Head from "../components/head";
+
 function AboutPage(){
     return <main>
+
         <title>자기소개</title>
 
-        <div style={{display:"flex",gap:20}}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/">홈</Link>
-</div>
+        <Head /> 
+
         <h1>자기소개 페이지입니다.</h1>
     </main>
 }
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,17 +1,14 @@
 import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
+
 
 function TestPage(){
     return <main>
         <title>test</title>
 
 
-<div style={{display:"flex",gap:20}}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/">홈</Link>
-</div>
+        <Head /> 
             
 
         <h1>테스트 페이지입니다.</h1>
