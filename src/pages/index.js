4fbf21c7233632f4851e49d83c22f9a4508e8a7d
hyujikoh <dvum0045@gmail.com>
2022-07-24 17:59:20 +0900

    홈페이지 메인페이지 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,13 @@
-import { StaticImage } from "gatsby-plugin-image";
 import React from "react"
-<<<<<<< HEAD
-const IndexPage =() =>{
-  return (
-<main>
-    <title>Home Page???</title>
-    <h1>Home Page!!!</h1>
-
-</main>
-=======
 import Layout from "../components/layout";
-const IndexPage =() =>{
-  return (
+function IndexPage(){
+  return (        
+  
+  <Layout pageTitle="Home Page">
+      <p>홈페이지 입니다</p>
+  </Layout>);
 
-      <Layout pageTitle="HomePage">
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-        <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      
-      </Layout>
-
->>>>>>> main
-
-  );
-};
+}
 
 
 export default IndexPage;
